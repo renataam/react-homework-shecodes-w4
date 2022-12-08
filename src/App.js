28617d11158b91a83f0a4763de92920fb533ead3
Renata Monteiro <108954513+renataam@users.noreply.github.com>
2022-12-08 18:38:33 -0300

    link github

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
     <div className="App mt-5">
       <h1>Weather App</h1>
       <Weather />
+      <div>
+        <a href="https://github.com/renataam/react-homework-shecodes-w4">
+          Open source
+        </a>
+      </div>
     </div>
   );
 }
